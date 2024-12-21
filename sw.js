15fe4c66b612a51a2ce3c9156926c1e6041ed29a
Tal Sagie <talsagie19@gmail.com>
2024-12-22 00:03:52 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "fitness-app-v1";
+const urlsToCache = [
+  "/fit-track/",
+  "/fit-track/index.html",
+  "/fit-track/manifest.json",
+  "/fit-track/favicon.ico",
+  "/fit-track/apple-touch-icon.png",
+  "/fit-track/icons/icon-192x192.png",
+  "/fit-track/icons/icon-512x512.png",
+  // Add other static assets
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
