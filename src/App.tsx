8e99ff1b8eec25feaf91b14604d7277e13682133
Tal Sagie <talsagie19@gmail.com>
2024-12-28 15:23:02 +0200

    chore : remove unnesscary react - qeury

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import MobileWorkoutPlan from "./pages/MobileWorkoutPlan";
@@ -14,8 +13,6 @@ import { useIsMobile } from "@/hooks/use-mobile";
 import { useAppDispatch } from "@/store/hooks";
 import { requestNotificationPermission } from "@/store/notificationSlice";
 
-const queryClient = new QueryClient();
-
 const App = () => {
   const dispatch = useAppDispatch();
   const isMobile = useIsMobile();
@@ -25,26 +22,24 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter basename="/fit-track">
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/workout-plan" element={<MobileWorkoutPlan />} />
-            <Route
-              path="/workout-library"
-              element={<WorkoutLibrary workouts={featuredWorkouts} />}
-            />
-            <Route path="/progress" element={<Progress />} />
-            {isMobile && (
-              <Route path="/exercises" element={<MobileExercises />} />
-            )}
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter basename="/fit-track">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/workout-plan" element={<MobileWorkoutPlan />} />
+          <Route
+            path="/workout-library"
+            element={<WorkoutLibrary workouts={featuredWorkouts} />}
+          />
+          <Route path="/progress" element={<Progress />} />
+          {isMobile && (
+            <Route path="/exercises" element={<MobileExercises />} />
+          )}
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
   );
 };
 
