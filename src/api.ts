2268e5162cc21532a4b78e3de9468957c403b722
Tal Sagie <talsagie19@gmail.com>
2025-01-04 20:23:52 +0200

    publish

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,7 @@
 import { Configuration, ExercisesApi } from "@forge-fit/server";
 
+console.log(import.meta.env.VITE_API_URL, "import.meta.env.VITE_API_URL");
+
 export const exercisesApi = new ExercisesApi(
   new Configuration({
     basePath: import.meta.env.VITE_API_URL,
